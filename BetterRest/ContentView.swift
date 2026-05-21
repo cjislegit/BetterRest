c741463505aa652e14c349d060f67a85d1136813
cjislegit <carlosjramirez11@gmail.com>
2026-05-20 20:00:19 -0700

    Refactored the code so bedTime in a computed property and is ran anytime one of the states is changed.

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -16,6 +16,29 @@ struct ContentView: View {
     @State private var alertMessage = ""
     @State private var showingAlert = false
     
+    var bedTime: String {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+                    let hour = (components.hour ?? 0) * 60 * 60
+                    let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(
+                        wake: Double(hour + minute),
+                        estimatedSleep: sleepAmount,
+                        coffee: Double(coffeeAmount)
+                    )
+
+                    let sleepTime = wakeUp - prediction.actualSleep
+
+                    return sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            return "Unable to calculate"
+        }
+    }
+    
     static var defaultWakeTime: Date {
         var componets = DateComponents()
         componets.hour = 7
@@ -45,39 +68,17 @@ struct ContentView: View {
                     .labelsHidden()
                 }
                 
+                Section ("Ideal bedtime is:") {
+                    Text(bedTime)
+                }
+                
                 
             }
             .navigationTitle("BetterRest")
-            .toolbar {
-                Button("Calculate", action: calculateBedtime)
-            }
-            .alert(alertTitle, isPresented: $showingAlert) {
-                Button("OK") { }
-            } message: {
-                Text(alertMessage)
-            }
+
         }
     }
     
-    func calculateBedtime() {
-        do {
-            let config = MLModelConfiguration()
-            let model = try SleepCalculator(configuration: config)
-            let compnents = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
-            let hour = (compnents.hour ?? 0) * 60 * 60
-            let minute = (compnents.minute ?? 0) * 60
-            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
-            let sleepTime = wakeUp - prediction.actualSleep
-            
-            alertTitle = "Your ideal bedtime is..."
-            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
-        } catch {
-            alertTitle = "Error"
-            alertMessage = "Sorry, there was a problem calculating your bedtime."
-        }
-        
-        showingAlert = true
-    }
 }
 
 #Preview {
